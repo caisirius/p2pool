a430aac3f2fc2702c2671d537aadf56b4ff71d90
can <can@can>
2019-11-11 10:12:51 +0800

    update freecash config

--- p2pool/bitcoin/networks/freecash.py
+++ p2pool/bitcoin/networks/freecash.py
@@ -12,7 +12,7 @@ P2P_PORT = 8333
 ADDRESS_VERSION = 35
 RPC_PORT = 8332
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            (yield helper.check_genesis_block(bitcoind, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
+            # (yield helper.check_genesis_block(bitcoind, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
             (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
@@ -20,9 +20,9 @@ POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
 SYMBOL = 'FCH'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Freecash') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Freecash/') if platform.system() == 'Darwin' else os.path.expanduser('~/.freecash'), 'freecash.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
-TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
-SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+BLOCK_EXPLORER_URL_PREFIX = 'https://freecash.info/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://freecash.info/address/'
+TX_EXPLORER_URL_PREFIX = 'https://freecash.info/tx/'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
